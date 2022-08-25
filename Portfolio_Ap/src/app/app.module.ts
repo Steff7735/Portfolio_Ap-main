9d287d3df2ed8a6938c6dd09840912968c1b38a3
Estefania Panza March <steff7735@gmail.com>
2022-08-25 01:55:40 -0300

    Optimización de Código

--- Portfolio_Ap/src/app/app.module.ts
+++ Portfolio_Ap/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { APLogoComponent } from './components/aplogo/aplogo.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -21,7 +20,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     APLogoComponent,
-    SocialComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
